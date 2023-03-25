7395b6d7a4ed98a83cd792e62a65e8710e0681e3
catmandoe1 <73958516+catmandoe1@users.noreply.github.com>
2023-03-25 03:50:53 +0000

    just need pages

--- scraper.py
+++ scraper.py
@@ -6,6 +6,7 @@ import mss.tools
 import numpy as np
 import sys
 import math
+import cv2
 
 SCREEN = 1 # 1 OR 2 (assuming 1920x1080 HD)
 
@@ -14,12 +15,25 @@ if SCREEN == 1:
 else:
     offset = SCREEN * 1920
 
-mouseDelay = 0.1
+MOUSEDELAY = 0.1
+MAX_IMAGE_WIDTH = 175
+MAX_IMAGE_HEIGHT = 175
+IMAGES_PER_ROW = 8
+IMAGES_PER_COLUMN = 6
+SAVEPATH = "C:/Programs/visual_studio_code/Python/gelbooruScaper/saves"
+
+# imageLocation = {
+#     "top": 343,
+#     "left": 262 + offset,
+#     "width": 1,
+#     "height": 3
+# }
+
 imageLocation = {
-    "top": 343,
-    "left": 262 + offset,
-    "width": 1,
-    "height": 3
+    "top": 341,
+    "left": 260 + offset,
+    "width": 1640,
+    "height": 691
 }
 
 pageContentLocation = {
@@ -30,7 +44,7 @@ pageContentLocation = {
 }
 
 def killSwitch():
-    raise Exception("Stopped!")
+    sys.exit("Stopped by user or Finished")
       
 def click(type = "left"):
     print("clicked")
@@ -52,20 +66,20 @@ def pressKey(key):
     
 def saveImage(savePath, nSaved, name, first = False):
     if first:
-        moveMouse(620 + offset, 55, mouseDelay) # path bar
+        moveMouse(620 + offset, 55, MOUSEDELAY) # path bar
         click()
         keyboard.write(savePath)
         pressKey("enter")
 
-        moveMouse(360 + offset, 440, mouseDelay) # file name bar
+        moveMouse(360 + offset, 440, MOUSEDELAY) # file name bar
         click()
         keyboard.write(f"{name} - {nSaved}")
 
-        moveMouse(780 + offset, 506, mouseDelay) # save button
+        moveMouse(780 + offset, 506, MOUSEDELAY) # save button
         click()
     else:
         keyboard.write(f"{name} - {nSaved}")
-        moveMouse(780 + offset, 506, mouseDelay) # save button
+        moveMouse(780 + offset, 506, MOUSEDELAY) # save button
         click()
 
 def openSaveMenu():
@@ -75,21 +89,31 @@ def openSaveMenu():
     time.sleep(0.25)
 
 def goIntoPic():
+    ss = mss.mss()
+    pre = np.array(ss.grab(imageLocation))
+    preSum = pre.sum()
+
     click()
-    moveMouse(262 + offset, 343, mouseDelay)
+    moveMouse(262 + offset, 343, MOUSEDELAY) # ready to save
+    time.sleep(0.4)
+    #moveMouse(10, 10) #moves cursur out of the way - nvm
 
     while True:
         ss = mss.mss()
         area = np.array(ss.grab(imageLocation))
-        print(area.sum())
-        print(area)
-        if area.sum() != 1044:
+        sum = area.sum()
+        #cv2.imshow("imggg", area)
+        #cv2.waitKey()
+        if sum != 394367520 and sum != preSum: # the number is equal to the sum of defualt background, when the image loads it changes
+            print("image loaded")
             break
+    
+    
 
 def checkIfPic(y, i):
     location = {
-    "top": 350 + (y % 3) * 200,
-    "left": (330 + i * 200) + offset,
+    "top": 384 + (y % 3) * 206,
+    "left": (337 + i * 195) + offset,
     "width": 1,
     "height": 3
     }
@@ -108,7 +132,7 @@ def checkIfPic(y, i):
 
 def addMinusVideoTag():
     print("adding \"-video\" tag...")
-    moveMouse(210 + offset, 240, mouseDelay) # tag bar
+    moveMouse(210 + offset, 240, MOUSEDELAY) # tag bar
     click()
     pressKey("end")
     pressKey("space")
@@ -127,7 +151,7 @@ def getNumFromUser(message):
             print("invalid number")
 
 def nextPage(page):
-    moveMouse(800 + offset, 65, mouseDelay) # url bar
+    moveMouse(800 + offset, 65, MOUSEDELAY) # url bar
     click()
     pressKey("end")
     keyboard.write(f"&pid={page * 42}")
@@ -136,13 +160,13 @@ def nextPage(page):
     
 
 def main():
-    imgsPerRow = 8#8
-    imgsPerColumn = 6#6
-    savePath = "C:/Programs/visual_studio_code/Python/gelbooruScaper/saves"
-    preSnipSum = -1
+
+    preSnip = cv2.cvtColor(cv2.imread("C:/Programs/visual_studio_code/Python/gelbooruScaper/blank.jpg"), cv2.COLOR_BGR2GRAY)
+    
 
     print("MAKE SURE URL DOESNT HAVE &pid=[x] IN IT")
     print("press \"alt+q\" to stop")
+    print("choosen screen =", SCREEN)
 
     keyboard.add_hotkey("alt+q", killSwitch)
 
@@ -151,37 +175,54 @@ def main():
     amount = getNumFromUser("enter amount of images to scrape (-1 = infinite): ")
 
     if amount >= 0:
-        pages = math.ceil(amount / (imgsPerRow * imgsPerColumn))
+        pages = math.ceil(amount / (IMAGES_PER_ROW * IMAGES_PER_COLUMN))
     else:
         pages = sys.maxsize * 2 + 1 # big number !!!
 
-    addMinusVideoTag()
+    moveMouse(1920 / 2, 1080 / 2, MOUSEDELAY)
     mouse.wheel(800)
+    addMinusVideoTag()
+    
 
     if pages == 0:
         raise Exception("cant scrape 0 pages")
 
     for page in range(0, pages):
-        for y in range(0, imgsPerColumn):
+        for y in range(0, IMAGES_PER_COLUMN):
             if y == 3:
+                time.sleep(1)
                 mouse.wheel(-200)
-                time.sleep(0.4)
+                time.sleep(1)
 
-            for i in range(0, imgsPerRow):
+            for i in range(0, IMAGES_PER_ROW):
 
                 if y == 5 and i == 2:
                     break # last picture normally
+
+                if saved >= amount:
+                    killSwitch()
 
                 # seeing if page has changed
                 while True:
                     ss = mss.mss()
                     ss = np.array(ss.grab(pageContentLocation))
+                    
+                    ss = cv2.cvtColor(ss, cv2.COLOR_BGR2GRAY)
+                    #cv2.imshow("ss", ss)
+                    #cv2.imshow("pre", preSnip)
+                    cv2.waitKey()
 
-                    if ss.sum() != preSnipSum:
+                    result = cv2.matchTemplate(ss, preSnip, cv2.TM_CCORR_NORMED)
+                    _, max, _, _ = cv2.minMaxLoc(result)
+                    print("max:", max)
+
+                    if max < 0.90:
+                        cv2.imwrite(f"C:/Programs/visual_studio_code/Python/gelbooruScaper/debug_out/loadedSnip-{y}{i}.jpg", ss)
+                        print("page loaded")
                         break
 
-                moveMouse((330 + i * 200) + offset, 350 + (y % 3) * 200, mouseDelay) # hover on the pic
-                print(str((330 + i * 200) + offset), str( 350 + (y % 3) * 200))
+                moveMouse((337 + i * 195) + offset, 384 + (y % 3) * 206, MOUSEDELAY) # hover on the pic
+                print(f"current location x:{(337 + i * 195) + offset} y:{384 + (y % 3) * 206}")
                 
                 checkIfPic(y, i)
 
@@ -190,17 +231,39 @@ def main():
                 openSaveMenu()
 
                 if y == 0 and i == 0:
-                    saveImage(savePath, saved, currentName, True)
-                saveImage(savePath, saved, currentName)
+                    saveImage(SAVEPATH, saved, currentName, True)
+                saveImage(SAVEPATH, saved, currentName)
                 saved = saved + 1
-                pressKey("alt+left_arrow")
                 
+                time.sleep(0.5) # allows for the save window to close in time
                 # used for checking if page as changed
                 ss = mss.mss()
                 ss = np.array(ss.grab(pageContentLocation))
-                preSnipSum = ss.sum()
+                preSnip = cv2.cvtColor(ss, cv2.COLOR_BGR2GRAY)
+                cv2.imwrite(f"C:/Programs/visual_studio_code/Python/gelbooruScaper/debug_out/preSnip-{y}{i}.jpg", ss)
+                pressKey("alt+left_arrow")
 
-
-                time.sleep(1.25)
-        nextPage(page + 1)
+                
+        nextPage(page + 1) # TODO FIX
 main()
+
+# img = np.array(cv2.imread("C:/Programs/visual_studio_code/Python/gelbooruScaper/debug_out/preSnip-30.jpg"))
+# img2 = np.array(cv2.imread("C:/Programs/visual_studio_code/Python/gelbooruScaper/debug_out/loadedSnip-30.jpg"))
+
+# print(img.sum(), img2.sum())
+
+# if img.sum() == img2.sum():
+#     print("img = img2")
+# else:
+#     print("img != img2")
+
+# img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+# result = cv2.matchTemplate(img, img2, cv2.TM_CCORR_NORMED)
+# cv2.imshow("igmgg", result)
+# cv2.waitKey()
+
+# min, max, minLoc, maxLoc = cv2.minMaxLoc(result)
+
+# print(min, max, minLoc, maxLoc)
